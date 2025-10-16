d5096323bca3d5b2ccdf6fa4d620b197ed2ca963
Vismaya-a <116067976+Vismaya-a@users.noreply.github.com>
2025-10-16 12:35:09 +0530

    Delete damage_location.py

--- damage_location.py
+++
@@ -1,9 +0,0 @@
-from models import location_model
-from utils.preprocessing import preprocess_image
-
-def classify_damage_location(image_path):
-    preprocessed_img = preprocess_image(image_path)
-    prediction = location_model.predict(preprocessed_img)
-    class_labels = ['front', 'back', 'rear']
-    predicted_class = np.argmax(prediction)
-    return class_labels[predicted_class]
